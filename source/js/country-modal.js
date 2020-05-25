2cad657a14ac33f3d100bf33c32c3930781ee42d
Мария <slinkywizard@mail.ru>
2020-05-25 20:17:41 +0300

    Выравнивание, мобильных меню, планшетных меню, модального окна, всплывающего блока-фильтра со странами

--- source/js/country-modal.js
+++ source/js/country-modal.js
@@ -1,4 +1,4 @@
-var button = document.querySelector(".new-destinations__country-button--open");
+var button = document.querySelector(".new-destinations__country-button--plus");
 
 var popup = document.querySelector(".alphabet-country-block");
 
@@ -6,19 +6,19 @@ var close = popup.querySelector(".new-destinations__country-button--close");
 
 button.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.add("new-destinations__alphabet-country-block--modal-show");
+  popup.classList.add("alphabet-country-block--modal-show");
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
-  popup.classList.remove("new-destinations__alphabet-country-block--modal-show");
+  popup.classList.remove("alphabet-country-block--modal-show");
 });
 
 window.addEventListener("keydown", function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
-    if (popup.classList.contains("new-destinations__alphabet-country-block--modal-show")) {
-      popup.classList.remove("new-destinations__alphabet-country-block--modal-show");
+    if (popup.classList.contains("alphabet-country-block--modal-show")) {
+      popup.classList.remove("alphabet-country-block--modal-show");
     }
   }
 });
